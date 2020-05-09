ff4531aff108e353cb6f23247cf3ca127bf082f1
ilhamuh97 <ilhamuh97@gmail.com>
2020-05-09 15:44:37 +0200

    add biodata

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './container.scss'
 
 import Homepage from '../homepage/homepage'
+import Biodata from '../biodata/biodata'
 
 const Container = () => {
     return (
         <div className="container">
             <Homepage />
+            <Biodata />
         </div>
     );
 };
