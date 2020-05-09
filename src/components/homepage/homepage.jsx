7928afe6e2872c8ebc6cee7840b199869d4c2793
ilhamuh97 <ilhamuh97@gmail.com>
2020-05-09 15:44:51 +0200

    improve homepage

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -9,8 +9,8 @@ const Homepage = () => {
             <div className="hero-content">
                 <div className="">
                     <h1 className="welcome-text">Hey There!</h1>
-                    <h2 className="my-name-text">I am <span>Ilham Muhammad</span></h2>
-                    <p className="description-text">and I am a media informatics student in Berlin</p>
+                    <h2 className="my-name-text">I am <span>Ilham</span></h2>
+                    <p className="description-text"><i>and I am a media informatics student in Berlin</i></p>
                     <div className="button-holder">
                         <button>See More</button>
                     </div>
