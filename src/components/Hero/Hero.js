6afe4b0e5d6f983383b625af59b025079c6d60f6
ilhamuh97 <33753707+ilhamuh97@users.noreply.github.com>
2022-12-28 20:31:39 +0100

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
     const heroText = {
         name: "Hi, I'm Ilham",
         shortStory:
-            "As a frontend website developer, I specialize in creating visually appealing and user-friendly websites. I am also interested in incorporating visual computing techniques to enhance the look and feel of my websites.",
+            "I am a frontend website developer with a passion for creating visually appealing and user-friendly websites. In addition to my development skills, I am interested in visual computing, including computer vision, image processing, and data visualization.",
     };
     return (
         <header className="hero">
