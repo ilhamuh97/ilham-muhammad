fea838e8f376579f222615e6ba38eaf9747dfc9b
ilhamuh97 <ilhamuh97@gmail.com>
2023-04-25 20:48:00 +0200

    update projects

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,15 +1,14 @@
 export const projectList = {
     list: [
         {
-            title: "Sate Somay",
-            type: "Volunteer Project",
+            title: "Sate Somay E-Commerce",
+            type: "Volunteering Project",
             year: "November 2021 - present",
             description:
-                "This website was created to provide an ordering system for food or drinks traded at the annual food festival organized by IWKZ e.V. called SASO.\n\nThe website was first launched at the SASO 2022 event, and I am responsible as a frontend developer on this project.\n\nCurrently, this project has been temporarily suspended because there are many things that are still under discussion for further development.",
+                "This website was created to provide an ordering system for food or drinks traded at the annual food festival organized by IWKZ e.V. called SASO.\n\nThe website was first launched at the SASO 2022 event, and I am responsible as a frontend developer on this project.",
             techStack: [
                 "Next.js",
-                "Tailwind",
-                "Material UI",
+                "Ant Design",
                 "Express.js",
                 "MongoDB",
                 "Docker",
@@ -19,8 +18,8 @@ export const projectList = {
         },
         {
             title: "ClassifyAI",
-            type: "Independent Project",
-            year: "November 2022 - present",
+            type: "Independent Coursework Project",
+            year: "November 2022 - February 2023",
             description: `The application provides a user-friendly interface for creating models for image classification without the need for coding or deep learning expertise. Key achievements of the application include its ease of use and accessibility for users of all skill levels. The application has a responsive design, which enables it to be accessed via mobile phones as well.\n
 The process of creating a model for image classification using the ClassifyAI web-based application involves four key steps:\n
 1. Class Creation and Data Collection
@@ -37,7 +36,7 @@ The application utilizes several javascript-based libraries, including React for
             ],
             source: [
                 {
-                    url: "https://classify-ai.netlify.app/",
+                    url: "https://classify-ai.vercel.app/",
                     text: "ClassifyAI",
                 },
             ],
@@ -45,11 +44,11 @@ The application utilizes several javascript-based libraries, including React for
         },
         {
             title: "Other Projects",
-            type: "Mini Project at University",
-            year: "December",
+            type: "Mini Projects at University",
+            year: "December 2022",
             description:
                 "These are mini projects that I did at the University. Most of them are Visual Computing implementations on the web.",
-            techStack: ["SVG.js", "Chart.js", "p5.js"],
+            techStack: ["Vanilla.js", "SVG.js", "Chart.js", "p5.js"],
             source: [
                 {
                     url: "https://ilhamuh97.github.io/hanoi-visualization/",
